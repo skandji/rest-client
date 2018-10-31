a6fac216495c61978c83aced59ef4eefdf8bd28b
Kandji <kandjimbacke@gmail.com>
2018-10-31 17:15:58 +0000

    Second Commit

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,30 +5,35 @@ export const PERSONS: Personne[] = [
     id: 1,
     prenom: 'Demba',
     nom: 'SOW',
-    email: 'dsow@get.com'
+    email: 'dsow@get.com',
+    classe: null
   },
   {
     id: 2,
     prenom: 'Demba2',
     nom: 'SOW2',
-    email: 'dsow2@get.com'
+    email: 'dsow2@get.com',
+    classe: null
   },
   {
     id: 3,
     prenom: 'Demba3',
     nom: 'SOW3',
-    email: 'dsow3@get.com'
+    email: 'dsow3@get.com',
+    classe: null
   },
   {
     id: 4,
     prenom: 'Demba4',
     nom: 'SOW4',
-    email: 'dsow4@get.com'
+    email: 'dsow4@get.com',
+    classe: null
   },
   {
     id: 5,
     prenom: 'Demba5',
     nom: 'SOW5',
-    email: 'dsow5@get.com'
+    email: 'dsow5@get.com',
+    classe: null
   },
 ];
